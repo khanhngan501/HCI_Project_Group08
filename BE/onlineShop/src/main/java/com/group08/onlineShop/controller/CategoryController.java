117bdc9bced3439e95dd0f1d33e51f5c17bc61ee
minhhoang1511sn <80461876+minhhoang1511sn@users.noreply.github.com>
2023-05-12 11:24:48 +0700

    sa

--- BE/onlineShop/src/main/java/com/group08/onlineShop/controller/CategoryController.java
+++ BE/onlineShop/src/main/java/com/group08/onlineShop/controller/CategoryController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("api/v1")
 @RequiredArgsConstructor
+@CrossOrigin(origins ="http://localhost:3000")
 public class CategoryController {
     private final CategoryService categoryService;
 
